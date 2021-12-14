b87b578fff8cd88895db5a30321dde717a404402
Donny Lawrence <trea@treamous.com>
2021-12-14 13:50:02 -0500

    debug

--- apps/Steam Link/run.sh
+++ apps/Steam Link/run.sh
@@ -1,5 +1,8 @@
 ( bash /home/pi/4180HomeSteamMachine/apps/Steam\ Link/run2.sh & )
+touch 1
 killall --signal SIGKILL python3
+touch 2
 killall --signal SIGKILL xinit
+touch 3
 killall xow
 killall Xorg
