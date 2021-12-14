6465b00de0544cbe8debdc895c97938a2a8d3656
Donny Lawrence <trea@treamous.com>
2021-12-14 11:57:32 -0500

    scrolling bg

--- menu/main.py
+++ menu/main.py
@@ -4,9 +4,12 @@ import subprocess
 import functools
 
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.uix.button import Button
+from kivy.uix.image import Image
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
+from kivy.graphics import Color, Rectangle
 
 buttons = []
 test_label = None
@@ -17,12 +20,22 @@ class GameButton(Button):
         subprocess.run(['bash', f'../apps/{self.text}/run.sh'])
 
 class Menu(Widget):
+    def move_bg(self, dt):
+        self.bg_texture.uvpos = (self.bg_texture.uvpos[0] - (dt / 2), self.bg_texture.uvpos[1] + (dt / 2))
+        self.canvas.before.clear()
+        with self.canvas.before:
+            Color(rgba=(1,1,1,0.3))
+            Rectangle(texture=self.bg_texture, size=self.size, pos=self.pos)
+
     def __init__(self, **kwargs):
+        self.bg_texture = Image(source="bg.png").texture
+        self.bg_texture.wrap = 'repeat'
+        self.bg_texture.uvsize = ((1920 / 1080) * 8, -8)
+
         super().__init__(**kwargs)
 
         gamelist = self.ids.gamelist
 
-        first = True
         for f in os.listdir("../apps"):
             if not os.path.isfile(f):
                 button = GameButton(text=f, height=80, size_hint_y=None)
@@ -33,6 +46,8 @@ class Menu(Widget):
 
         global test_label
         test_label = self.ids.testlabel
+
+        Clock.schedule_interval(self.move_bg, 0)
 
 
 class MenuApp(App):
@@ -69,17 +84,12 @@ def on_joy_hat(win, stickid, axisid, value):
     elif value[1] == -1:
         selection_down()
 
-def on_joy_axis(win, stickid, axisid, value):
-    test_label.text = f'axisid: {str(axisid)} value: {str(value)}'
+def on_joy_button_down(win, stickid, axisid, value):
+    pass
 
-    if axisid != 1:
-        return
+def on_joy_button_up(win, stickid, axisid, value):
+    pass
 
-    if value > 500:
-        selection_up()
-    elif value < -500:
-        selection_down()
-
-Window.bind(on_joy_hat=on_joy_hat, on_joy_axis=on_joy_axis)
+Window.bind(on_joy_hat=on_joy_hat, on_joy_button_down=on_joy_button_down, on_joy_button_up=on_joy_button_up)
 
 MenuApp().run()
